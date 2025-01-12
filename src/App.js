f7223a190c84bde671f1d4d57e9c37c7f8360802
shailesh kaiwart <kaiwartshailesh@gmail.com>
2025-01-12 20:18:15 +0530

    feature: dashboard added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SearchResultInCard from "./pages/SearchResultInCard/SearchResultInCard";
 import SearchResultLayout from "./pages/SearchResultLayout/SearchResultLayout";
 import DynamicTable from "./components/DynamicTable/DynamicTable";
 import Login from "./pages/login/Login";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 import "./App.css";
 // import "./App_copy.css";
@@ -18,9 +19,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         
         <Route path="/" element={<MainLayout />}>
-          <Route index element={<SearchResultLayout />} />
-          <Route path="table-view" element={<DynamicTable />} />
-          {/* <Route index element={<SearchResultInCard />} /> */}
+          <Route index element={<Dashboard />} />
+          <Route path="search-result" element={<SearchResultLayout />} />
         </Route>
       </Routes>
     </Router>
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dashboard = () => (
+  <div className="dashboard">
+    <h3>Welcome to dashboard</h3>
+  </div>
+);
+
+export default Dashboard;
